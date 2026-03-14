077716e71bb8870b493c14c04fcd4784dfdece11
pale1223 <kanghyun1223@gmail.com>
2026-03-15 02:04:31 +0900

    test(concrete_types): 복소수 연산자 활용 예시 추가

--- Language/A Tour of C++/4 Classes/concrete_types.cpp
+++ Language/A Tour of C++/4 Classes/concrete_types.cpp
@@ -44,3 +44,13 @@ bool operator!=(complex a, complex b) // not equal
 {
     return !(a == b);
 }
+
+void f(complex z)
+{
+    complex a {2.3}; // construct {2.3, 0.0} from 2.3
+    complex b {1 / a};
+    complex c {a + z * complex{1, 2.3}};
+    // ...
+    if (c != b)
+        c = -(b / a) + 2 * b;
+}
