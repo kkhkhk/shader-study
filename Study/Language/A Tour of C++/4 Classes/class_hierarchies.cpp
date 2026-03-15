5a322dfa557bfd34c00cc3c5d99114b88a853fa6
pale1223 <kanghyun1223@gmail.com>
2026-03-16 02:09:14 +0900

    feat(Smiley): Circle 클래스를 상속받는 "웃는 얼굴" 클래스 구현

--- Study/Language/A Tour of C++/4 Classes/class_hierarchies.cpp
+++ Study/Language/A Tour of C++/4 Classes/class_hierarchies.cpp
@@ -41,4 +41,38 @@ public:
     void rotate(int) {}
 };
 
+class Smiley : public Circle // use the circle as the base for a face
+{
+private:
+    vector<Shape *> eyes;
+    Shape *mouth;
 
+public:
+    Smiley(Point p, int r) : Circle{p, r}, mouth{nullptr} {}
+
+    ~Smiley()
+    {
+        delete mouth;
+        for (auto p : eyes)
+            delete p;
+    }
+
+    void move(Point to);
+
+    void draw() const;
+    void rotate(int);
+
+    void add_eye(Shape *s) { eyes.push_back(s); }
+    void set_mouth(Shape *s);
+    virtual void wink(int i);       // wink eye number i 
+
+    // ...
+};
+
+void Smiley::draw() const
+{
+    Circle::draw();
+    for (auto p : eyes)
+        p->draw();
+    mouth->draw();
+}
