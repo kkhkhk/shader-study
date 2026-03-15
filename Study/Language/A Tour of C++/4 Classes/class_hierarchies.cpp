0ddd9e4e1bb8303e4ef56d92376a7441990622bc
pale1223 <kanghyun1223@gmail.com>
2026-03-16 02:41:33 +0900

    test(user): read_shape 함수를 사용하는 테스트 코드 구현

--- Study/Language/A Tour of C++/4 Classes/class_hierarchies.cpp
+++ Study/Language/A Tour of C++/4 Classes/class_hierarchies.cpp
@@ -106,3 +106,14 @@ Shape *read_shape(istream& is) // read shape descriptions from input stream is
         return ps;
     }
 }
+
+void user()
+{
+    vector<Shape *> v;
+    while (cin) 
+        v.push_back(read_shape(cin));
+    draw_all(v);            // call draw() for each element
+    rotate_all(v, 45);      // call rotate(45) for each element
+    for (auto p : v)        // remember to delete elements
+        delete p;
+}
