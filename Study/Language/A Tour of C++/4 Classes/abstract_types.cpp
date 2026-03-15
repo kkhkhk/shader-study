083188a878b7a3a48c7012b2af3173aa6c8f8530
pale1223 <kanghyun1223@gmail.com>
2026-03-15 21:09:06 +0900

    feat(Container): 추상 클래스 Container 추가

---
+++ Study/Language/A Tour of C++/4 Classes/abstract_types.cpp
@@ -0,0 +1,8 @@
+class Container
+{
+public:
+    virtual double &operator[](int) = 0; // pure virtual function
+    virtual int size() const = 0;        // const member function
+    virtual ~Container() {}              // destructor
+};
+
