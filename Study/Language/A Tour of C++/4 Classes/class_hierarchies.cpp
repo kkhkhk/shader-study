68daca4649d9a663969c457b72a6f97a5558c35f
pale1223 <kanghyun1223@gmail.com>
2026-03-16 02:39:22 +0900

    feat(read_shape): 주어진 인풋 스트림에서 도형의 정보를 추출하여 해당 도형의 객체를 반환하는 함수 구현

--- Study/Language/A Tour of C++/4 Classes/class_hierarchies.cpp
+++ Study/Language/A Tour of C++/4 Classes/class_hierarchies.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -75,4 +76,33 @@ void Smiley::draw() const
     for (auto p : eyes)
         p->draw();
     mouth->draw();
+}
+
+enum class Kind
+{
+    circle,
+    triangle,
+    smiley
+};
+
+Shape *read_shape(istream& is) // read shape descriptions from input stream is
+{
+    // ... read shape header from is and find its Kind k ...
+
+    switch (k)
+    {
+    case Kind::circle:
+        // read circle data {Point, int} into p and r
+        return new Circle{p, r};
+    case Kind::triangle:
+        // read triangle data {Point, Point, Point} into p1, p2, and p3
+        return new Triangle{p1, p2, p3};
+    case Kind::smiley:
+        // read smiley data {Point, int, Shape, Shape, Shape} into p, r, e1, e2, and m
+        Smiley *ps = new Smiley{p, r};
+        ps->add_eye(e1);
+        ps->add_eye(e2);
+        ps->set_mouth(m);
+        return ps;
+    }
 }
