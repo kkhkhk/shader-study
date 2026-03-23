39cbf9b77c335fcecdbfc41660ee4bb00110c404
pale1223 <kanghyun1223@gmail.com>
2026-03-24 07:51:54 +0900

    test(user): sum 템플릿을 사용하는 예시 코드 구현

--- Study/Language/A Tour of C++/5 Templates/function_templates.cpp
+++ Study/Language/A Tour of C++/5 Templates/function_templates.cpp
@@ -1,3 +1,11 @@
+#include <list>
+#include <vector>
+#include <complex>
+
+#include <parameterized_types.cpp>
+
+using namespace std;
+
 template<typename Container, typename Value>
 Value sum(const Container &c, Value v)
 {
@@ -5,3 +13,11 @@ Value sum(const Container &c, Value v)
         v += x;
     return v;
 }
+
+void user(Vector<int> &vi, list<double> &ld, vector<complex<double>> &vc)
+{
+    int x = sum(vi, 0);                  // the sum of a vector of ints (add ints)
+    double d = sum(vi, 0.0);             // the sum of a vector of ints (add doubles)
+    double dd = sum(ld, 0.0);            // the sum of a list of doubles
+    auto z = sum(vc, complex<double>{}); // the sum of a vector of complex<double>
+}                                        // the initial value is {0.0, 0.0}
