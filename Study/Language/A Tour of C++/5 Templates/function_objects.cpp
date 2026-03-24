75c624a1cf966171898e6f6eedd59f856a15cb7f
pale1223 <kanghyun1223@gmail.com>
2026-03-25 06:12:19 +0900

    test(fct): Less_than functor를 활용하는 예시 코드 구현

--- Study/Language/A Tour of C++/5 Templates/function_objects.cpp
+++ Study/Language/A Tour of C++/5 Templates/function_objects.cpp
@@ -1,3 +1,7 @@
+#include <string>
+
+using namespace std;
+
 template <typename T>
 class Less_than
 {
@@ -6,3 +10,11 @@ public:
     Less_than(const T &v) : val(v) {}
     bool operator()(const T &x) const { return x < val; } // call operator
 };
+
+void fct(int n, const string &s)
+{
+    Less_than<int> lti{42};             // lti(i) will compare i to 42 using < (i < 42)
+    Less_than<string> lts{"Backus"};    // lts(s) will compare s to 'Backus' using < (s < 'Backus')
+    bool b1 = lti(n);
+    bool b2 = lts(s);
+}
