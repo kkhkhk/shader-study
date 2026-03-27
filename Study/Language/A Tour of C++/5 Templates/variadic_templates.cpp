5ddf74cc04e1743a8c2fbdb106fcc8565cb2f585
pale1223 <kanghyun1223@gmail.com>
2026-03-28 02:37:40 +0900

    fix(f): 생략 부호 추가

--- Study/Language/A Tour of C++/5 Templates/variadic_templates.cpp
+++ Study/Language/A Tour of C++/5 Templates/variadic_templates.cpp
@@ -14,7 +14,7 @@ template <typename T, typename... Tail>
 void f(T head, Tail... tail)
 {
     g(head);    // do something to head
-    f(tail);    // try again with tail
+    f(tail...);    // try again with tail
 }
 
 int main()
