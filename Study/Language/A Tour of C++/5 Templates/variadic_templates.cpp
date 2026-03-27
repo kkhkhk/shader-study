876e89af9139e3520f98bcc1abc47fc0183110f3
pale1223 <kanghyun1223@gmail.com>
2026-03-28 02:36:11 +0900

    test(f): 가변 템플릿 동작 확인

--- Study/Language/A Tour of C++/5 Templates/variadic_templates.cpp
+++ Study/Language/A Tour of C++/5 Templates/variadic_templates.cpp
@@ -16,3 +16,13 @@ void f(T head, Tail... tail)
     g(head);    // do something to head
     f(tail);    // try again with tail
 }
+
+int main()
+{
+    cout << "first: ";
+    f(1, 2.2, "hello");
+
+    cout << "\nsecond: ";
+    f(0.2, 'c', "yuck!", 0, 1, 2);
+    cout << "\n";
+}
