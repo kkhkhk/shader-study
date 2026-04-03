5a094bb6cef4d0d45490c181e59c39c57bf7e4e9
pale1223 <kanghyun1223@gmail.com>
2026-04-04 00:20:02 +0900

    feat(use2): 파일 입력 스트림 및 정규표현식 서브 그룹 매칭 기능 추가

--- Study/Language/A Tour of C++/7 Strings and Regular Expressions/regular_expressions.cpp
+++ Study/Language/A Tour of C++/7 Strings and Regular Expressions/regular_expressions.cpp
@@ -1,6 +1,7 @@
 #include <regex>
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -16,5 +17,27 @@ void use1()
         smatch matches;
         if (regex_search(line, matches, pat1))
             cout << lineno << ":" << matches[0] << "\n";
+    }
+}
+
+void use2()
+{
+    ifstream in("file.txt");
+    if (!in)
+        cerr << "no file\n";
+
+    regex pat {R"(\w{2}\s*\d{5}(-\d{4})?)"};    // U.S. postal code pattern
+
+    int lineno = 0;
+    for (string line; getline(in, line);)
+    {
+        ++lineno;
+        smatch matches;     // matched strings go here
+        if (regex_search(line, matches, pat))
+        {
+            cout << lineno << ": " << matches[0] << "\n";   // the complete match
+            if (1 < matches.size() && matches[1].matched)
+                cout << "\t" << matches[1] << "\n";
+        }
     }
 }
