065712f40e75d6145010649a28d24be069b15962
pale1223 <kanghyun1223@gmail.com>
2026-04-04 00:03:28 +0900

    feat(use1): regex_search를 이용해 패턴 매치 라인 출력

--- Study/Language/A Tour of C++/7 Strings and Regular Expressions/regular_expressions.cpp
+++ Study/Language/A Tour of C++/7 Strings and Regular Expressions/regular_expressions.cpp
@@ -1,6 +1,20 @@
 #include <regex>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 regex pat1 (R"(\w{2}\s*\d{5}(-\d{4})?)"); // US postal code pattern: XXddddd-dddd and variants
 regex pat2 {"\\w{2}\s*\\d{5}(-\\d{4})?"}; // U.S. postal code pattern
+
+void use1()
+{
+    int lineno = 0;
+    for (string line; getline(cin, line);)
+    {
+        ++lineno;
+        smatch matches;
+        if (regex_search(line, matches, pat1))
+            cout << lineno << ":" << matches[0] << "\n";
+    }
+}
