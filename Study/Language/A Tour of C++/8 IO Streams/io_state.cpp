b79adac866a3f43ebf46c5e5304b5df4ba8f99c2
pale1223 <kanghyun1223@gmail.com>
2026-04-12 21:36:39 +0900

    test(istream): eof와 fail을 활용한 입력 스트림 상태 처리 예시 추가

--- Study/Language/A Tour of C++/8 IO Streams/io_state.cpp
+++ Study/Language/A Tour of C++/8 IO Streams/io_state.cpp
@@ -11,3 +11,38 @@ vector<int> read_ints(istream &is)
         res.push_back(i);
     return res;
 }
+
+int main()
+{
+    while (cin) 
+    {
+        for (int i; cin >> i;)
+        {
+            // ... use the integer ...
+        }
+
+        if (cin.eof())
+        {
+            // all is well we reached the end-of-file ...
+        }
+        else if (cin.fail())    // a potentially recoverable error
+        {
+            cin.clear(); // reset the state to good()
+            char ch;
+            if (cin >> ch)  // look for nesting represented by { ... }
+            {
+                switch (ch) 
+                {
+                case '{':
+                    // ... start nested structure ...
+                    break;
+                case '}':
+                    // ... end nested structure ...
+                    break;
+                default:
+                    cin.setstate(ios_base::failbit);    // add fail() to cin's state
+                }
+            }
+        }
+    }
+}
